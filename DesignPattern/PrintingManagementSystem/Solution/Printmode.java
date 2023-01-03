37006ad99aa3843e9c7df83885b689a25cf41853
Rufidatul Radium <60069688+Rufidatul726@users.noreply.github.com>
2023-01-03 15:36:40 +0600

    Add files via upload

---
+++ DesignPattern/PrintingManagementSystem/Solution/Printmode.java
@@ -0,0 +1,24 @@
+package Solution;
+
+public class Printmode {
+    Printmode mode;
+
+    private int number_of_pages;
+    private int page_size;
+    private String orientation;
+    private int color_intensity;
+    private int cost_per_page;
+
+    public Printmode(){
+        getMode();
+    }
+    public Printmode getMode() {
+        //mode= BooosterMode();
+        return mode;
+    }
+
+    Printmode BooosterMode(){
+        list<String>ColorIntesity={"High", "Medium", "Low"};
+    }
+
+}
