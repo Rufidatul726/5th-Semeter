bab0867c620c0b4d85deaf068a00367a3a466f82
Rufidatul Radium <60069688+Rufidatul726@users.noreply.github.com>
2023-02-12 15:30:21 +0600

    Add files via upload

---
+++ WebTechnology/PHP/Class1.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+through post method
+Welcome <?php echo $_POST["Firstname"]; ?> <?php echo $_POST["Lastname"]; ?> <br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
---
+++ WebTechnology/PHP/index.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+        <p>
+        <?php 
+            $name="Aurchey";
+            $classmates = array("Farin","Sharif","Sadia");
+            $CGPA = array("Name1"=>3.56 , "Name2"=>3.48, "Name3"=>3.88 );
+            $CGname="Name2";
+            
+            echo "Welcome $name to IIT <br>";
+            echo "Your Classmates are $classmates[0] , $classmates[1] and $classmates[2]<br>";
+            echo "$CGname has a cgpa of $CGPA[$CGname]";
+        ?>
+        </p>
+    <body>
+<html>
